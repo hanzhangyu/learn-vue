04b89f566c5fee015c65daf6742d8e214935a712
PaulHan <zhangyu.han@outlook.com>
2019-08-01 02:12:19 +0800

    feat: playground:lifeCycle vmCount

---
+++ playground/lifeCycle/index.js
@@ -0,0 +1,30 @@
+Vue.component("todo-item1", {
+    data() {
+        return this.todo; // 在父组件中的Ob对象中统计被其他 vm 当做 root 使用的 个数
+    },
+    props: ["todo"],
+    template: "<span>{{ todo.text }}</span>"
+});
+
+Vue.component("todo-item2", {
+    data() {
+        return this.todo;
+    },
+    props: ["todo"],
+    template: "<span>{{ todo.text }}</span>"
+});
+
+var app = new Vue({
+    data: {
+        todo: {
+            text: "nothing"
+        },
+    }
+});
+app.$mount('#app');
+
+console.log(app._data.todo.__ob__.vmCount); // 2
+console.log(app._data.todo === app.$children[0]._data); // true
+console.log(app._data.todo === app.$children[1]._data); // true
+app.$children[1].$destroy();
+console.log(app._data.todo.__ob__.vmCount); // 1
