4854db8739e8bce2a609a0f10fe0829bdcb08052
PaulHan <zhangyu.han@outlook.com>
2019-09-07 19:14:39 +0800

    feat: 尝试将proxy与data分离，感觉像是方向错了

--- reactive/observer-refactor/Observable.js
+++ reactive/observer-refactor/Observable.js
@@ -1,19 +1,64 @@
 /**
  * @typedef Watcher
- * @property {Function} update - callback.
+ * @property {function(any, any)} update - callback.
  */
+
+import { isObject } from "../observer/util.js";
 
 /**
  * Observable 类
  */
-export default class {
+export default class Observable {
     /**
      * collect watcher
      * @type {Array<Watcher>}
      */
     dep = [];
+    /**
+     * proxy for data
+     * @type {Proxy}
+     */
+    proxy = null;
 
     constructor(data, cb) {
+        this.proxy = this.walk(data);
+
+        // 暴露 this.proxy
+        return new Proxy(this, {
+            get: (target, propKey, value, receiver) => {
+                return (
+                    Reflect.get(this.proxy, propKey, receiver) || this[propKey]
+                );
+            },
+            set: (target, propKey, value, receiver) => {
+                return Reflect.set(this.proxy, propKey, value, receiver);
+            },
+        });
+    }
+
+    walk(data) {
+        console.log("data", data);
+        if (!isObject(data)) {
+            return;
+        }
+        const keys = Object.keys(data);
+        keys.forEach(key => {
+            data[key] = this.walk(data[key]); // fixme
+        });
+
+        return new Proxy(data, {
+            set: (target, propKey, value, receiver) => {
+                console.log(target, propKey, value, receiver);
+                this.dep.forEach(watcher =>
+                    watcher.update.call(
+                        this,
+                        value,
+                        Reflect.get(target, propKey),
+                    ),
+                );
+                return Reflect.set(target, propKey, value, receiver);
+            },
+        });
     }
 
     /**
@@ -21,6 +66,7 @@ export default class {
      * @param {Watcher} watcher
      */
     subsrible(watcher) {
+        if (this.dep.find(item => watcher === item)) return;
         this.dep.push(watcher);
     }
 }
