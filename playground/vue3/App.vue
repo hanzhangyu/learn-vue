a4199e76c6fe0873429608850e17cc5054be5b6e
PaulHan <zhangyu.han@outlook.com>
2019-11-16 19:46:34 +0800

    feat: vue3 test add to global

--- playground/vue3/App.vue
+++ playground/vue3/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        {{state}}
         <span>count is {{ state.count }}</span>
         <span>plusOne is {{ state.double }}</span>
         <button @click="increment">count++</button>
@@ -13,6 +14,11 @@ import useMousePosition from "./components/useMousePosition";
 
 export default {
     setup() {
+        Object.assign(window, {
+            reactive,
+            computed,
+        });
+        console.assert(this === undefined, "`this` is always be undefined");
         const { x, y } = useMousePosition();
 
         const state = reactive({
